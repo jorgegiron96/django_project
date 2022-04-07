d3187b6e3658ffa6a209e8520f6d72904766381c
Jorge <jorgegiron96@gmail.com>
2022-04-07 19:37:13 -0300

    ea

--- blog/views.py
+++ blog/views.py
@@ -31,9 +31,12 @@ class UserPostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 5
 
-    def get_query_set(self):
-        user = get_object_or_404(User, username = self.kwargs.get('username')
-        return Post.object.filter(author = user).order_by('-date_posted')
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+
 
 class PostDetailView(DetailView):
     model = Post

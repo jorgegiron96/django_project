869b30a1666d0111b1ad604366aea73a136f7f1a
Jorge <jorgegiron96@gmail.com>
2022-04-13 19:59:23 -0300

    NEW SECRET KEY

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qwnjm7*%a%^lufbfsa1p7li+v35a)veh*57pe6f@_y_!bgq2#@'
-
+# SECRET_KEY = 'django-insecure-qwnjm7*%a%^lufbfsa1p7li+v35a)veh*57pe6f@_y_!bgq2#@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['djangogiron.herokuapp.com']
 
